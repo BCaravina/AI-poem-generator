f1e1331e3db7a806444cd1e9fab4e688bfac0374
Biscatrix <168189345+Biscatrix@users.noreply.github.com>
2025-01-26 17:46:10 -0300

    Added radio buttons for different languages

--- src/script.js
+++ src/script.js
@@ -10,13 +10,20 @@ async function generatePoem(response) {
 async function getApiResponse(event) {
   event.preventDefault();
 
+  let selectedLanguage =
+    document.querySelector("input[type='radio'][name=language]:checked")
+      ?.value || "defaultLanguage";
+
   const apiKey = "c49ed490tce5aa3a51c741aaobee84ef";
   const context =
     "You are a poet that writes romantic poems no longer than 6 lines. Separate each line with a <br /> and do not include a title to the poem";
   const userInput = document.querySelector("#text-input");
-  let prompt = `Generate a poem in Brazilian Portuguese from the word ${userInput.value}`;
+  let prompt = `Generate a poem in ${selectedLanguage} from the word ${userInput.value}`;
   const apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   axios.get(apiUrl).then(generatePoem);
+  setTimeout(() => {
+    userInput.value = "";
+  }, 3000);
 }
 document.getElementById("poem-form").addEventListener("submit", getApiResponse);
