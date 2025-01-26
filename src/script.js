41f3e29b347d1a4f232489c4566760dac7aa5ca6
Biscatrix <168189345+Biscatrix@users.noreply.github.com>
2025-01-26 11:30:28 -0300

    Added JS form handling, minor HTML and CSS updates

--- src/script.js
+++ src/script.js
@@ -1,1 +1,15 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  //   let poemText = document.querySelector("#poem");
+  new Typewriter("#poem", {
+    strings: "La vie en rose",
+    autoStart: true,
+    delay: 60,
+    cursor: "",
+    loop: false,
+  });
+}
+
+let formElement = document.querySelector("#poem-form");
+formElement.addEventListener("submit", generatePoem);
