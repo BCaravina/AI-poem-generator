9c59e12a5b83af0327628efa62fdfbd8fc5fe209
Biscatrix <168189345+Biscatrix@users.noreply.github.com>
2025-01-26 16:34:57 -0300

    Imported Axios and Typerwriter plus expanded JS functionality

--- src/script.js
+++ src/script.js
@@ -1,15 +1,32 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+async function generatePoem(response) {
   //   let poemText = document.querySelector("#poem");
   new Typewriter("#poem", {
-    strings: "La vie en rose",
+    strings: response.data.answer,
     autoStart: true,
-    delay: 60,
+    delay: 20,
     cursor: "",
-    loop: false,
   });
 }
 
-let formElement = document.querySelector("#poem-form");
-formElement.addEventListener("submit", generatePoem);
+async function getApiResponse(event) {
+  event.preventDefault();
+
+  const apiKey = "c49ed490tce5aa3a51c741aaobee84ef";
+  const context =
+    "You are a poet that writes romantic poems no longer than 6 lines. Separate each line with a <br /> and do not include a title to the poem";
+  const userInput = document.querySelector("#text-input");
+  let prompt = `Generate a poem in Brazilian Portuguese from the word ${userInput.value}`;
+  const apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(generatePoem);
+
+  document.getElementById("poem").textContent = new Typewriter("#poem", {
+    strings: "Creating a poem for you...",
+    autoStart: true,
+    cursor: "",
+    loop: true,
+  });
+}
+document.getElementById("poem-form").addEventListener("submit", getApiResponse);
+// let formElement = document.querySelector("#poem-form");
+// formElement.addEventListener("submit", getApiResponse);
